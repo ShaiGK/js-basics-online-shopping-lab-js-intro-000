a929346b21791ab7d7906d2c4562138ac2cda384
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-06-21 18:13:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    return `Your total cost is $${amount}, which will be charged to the card ${cardNumber}.`
     var cart = []
+    return `Your total cost is $${amount}, which will be charged to the card ${cardNumber}.`
   }
 }
